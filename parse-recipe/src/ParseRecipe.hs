5b9137519a555d90410b7e2ec06782fccf9a9646
Jasper Woudenberg <mail@jasperwoudenberg.com>
2021-10-02 19:20:02 +0200

    Parse ingredients

--- parse-recipe/src/ParseRecipe.hs
+++ parse-recipe/src/ParseRecipe.hs
@@ -26,16 +26,44 @@ testFile :: FilePath
 testFile = "../../hiskevriendelijk/docs/hoofdgerechten/fesenjoon.md"
 
 data Recipe m = Recipe
-  { name :: m Text,
-    portions :: m Float,
-    ingredients :: m [Text],
+  { title :: m Text,
+    portions :: m Double,
+    ingredients :: m [Ingredient],
     instructions :: m [Text]
   }
+
+data Ingredient = Ingredient
+  { name :: Text,
+    quantity :: Maybe Quantity
+  }
+  deriving (Show)
+
+data Quantity = Quantity
+  { amount :: Double,
+    unit :: Maybe Unit
+  }
+  deriving (Show)
+
+data Unit
+  = Milli SiUnit
+  | Centi SiUnit
+  | Deci SiUnit
+  | Kilo SiUnit
+  | SiUnit SiUnit
+  | Eetlepel
+  | Theelepel
+  | Snuf
+  deriving (Show)
+
+data SiUnit
+  = Gram
+  | Liter
+  deriving (Show)
 
 extract :: Applicative g => (forall a. f a -> g (h a)) -> Recipe f -> g (Recipe h)
 extract f recipe =
   Recipe
-    <$> f (name recipe)
+    <$> f (title recipe)
     <*> f (portions recipe)
     <*> f (ingredients recipe)
     <*> f (instructions recipe)
@@ -44,18 +72,18 @@ parseBlock :: Block -> Recipe (Compose PandocIO Maybe) -> Recipe (Compose Pandoc
 parseBlock block recipe =
   case block of
     Header 1 _ inlines ->
-      recipe {name = Compose (fmap Just (toText [Plain inlines]))}
+      recipe {title = Compose (fmap Just (toText [Plain inlines]))}
     Para inlines ->
       recipe
         { portions = Compose $ do
             paragraphText <- toText [Plain inlines]
-            case P.parse parsePortions paragraphText of
-              P.Fail _ _ _ -> pure Nothing
-              P.Partial _ -> pure Nothing
-              P.Done _ portions' -> pure (Just portions')
+            pure $ P.maybeResult (P.parse portionsParser paragraphText)
         }
     BulletList items ->
-      recipe {ingredients = Compose (fmap Just (traverse toText items))}
+      recipe
+        { ingredients =
+            Compose $ fmap Just $ traverse (fmap parseIngredient . toText) items
+        }
     OrderedList _ items ->
       recipe {instructions = Compose (fmap Just (traverse toText items))}
     _ -> recipe
@@ -63,8 +91,92 @@ parseBlock block recipe =
 toText :: [Block] -> PandocIO Text
 toText blocks = fmap strip $ writePlain def (Pandoc mempty blocks)
 
-parsePortions :: P.Parser Float
-parsePortions =
+portionsParser :: P.Parser Double
+portionsParser =
   P.asciiCI "Voor "
     *> fmap realToFrac P.scientific
     <* P.choice [P.asciiCI " personen", P.asciiCI " persoon"]
+
+parseIngredient :: Text -> Ingredient
+parseIngredient ingredientText =
+  case P.parse (quantityParser <* P.space) ingredientText of
+    P.Fail _ _ _ -> Ingredient {name = ingredientText, quantity = Nothing}
+    P.Partial _ -> Ingredient {name = ingredientText, quantity = Nothing}
+    P.Done name' quantity' -> Ingredient {name = name', quantity = Just quantity'}
+
+quantityParser :: P.Parser Quantity
+quantityParser =
+  P.choice
+    [ Quantity <$> amountParser <* P.skipSpace <*> (fmap Just unitParser), -- 200 g bonen
+      Quantity <$> pure 1 <* P.skipSpace <*> (fmap Just unitParser), -- snufje zout
+      Quantity <$> amountParser <*> pure Nothing -- 2 paprikas
+    ]
+
+amountParser :: P.Parser Double
+amountParser =
+  P.choice
+    [ fractionParser,
+      fmap realToFrac P.scientific
+    ]
+
+fractionParser :: P.Parser Double
+fractionParser = do
+  enumerator <- P.scientific
+  P.skipSpace
+  _ <- P.string "/"
+  P.skipSpace
+  denumerator <- P.scientific
+  pure (realToFrac enumerator / realToFrac denumerator)
+
+unitParser :: P.Parser Unit
+unitParser =
+  P.choice
+    [ SiUnit <$> siUnitParser,
+      prefixedSiUnit Milli "milli",
+      prefixedSiUnit Milli "m",
+      prefixedSiUnit Centi "centi",
+      prefixedSiUnit Centi "c",
+      prefixedSiUnit Deci "deci",
+      prefixedSiUnit Deci "d",
+      prefixedSiUnit Kilo "kilo",
+      prefixedSiUnit Kilo "k",
+      optionallyPlural $ colloquial Eetlepel "eetlepel",
+      colloquial Eetlepel "el",
+      optionallyPlural $ colloquial Theelepel "theelepel",
+      colloquial Theelepel "tl",
+      optionallyDiminuitive $ colloquial Snuf "snuf"
+    ]
+
+prefixedSiUnit :: (SiUnit -> Unit) -> Text -> P.Parser Unit
+prefixedSiUnit ctor prefix =
+  ctor <$> (P.asciiCI prefix *> siUnitParser)
+
+colloquial :: Unit -> Text -> P.Parser Unit
+colloquial unit base =
+  pure unit <* P.asciiCI base
+
+optionallyPlural :: P.Parser a -> P.Parser a
+optionallyPlural parser =
+  P.choice
+    [ parser <* P.asciiCI "s",
+      parser
+    ]
+
+optionallyDiminuitive :: P.Parser a -> P.Parser a
+optionallyDiminuitive parser =
+  P.choice
+    [ parser <* P.asciiCI "jes",
+      parser <* P.asciiCI "je",
+      parser <* P.asciiCI "tjes",
+      parser <* P.asciiCI "tje",
+      parser
+    ]
+
+siUnitParser :: P.Parser SiUnit
+siUnitParser =
+  P.choice
+    [ P.asciiCI "gram" *> pure Gram,
+      P.asciiCI "g" *> pure Gram,
+      P.asciiCI "liter" *> pure Liter,
+      P.asciiCI "l" *> pure Liter
+    ]
